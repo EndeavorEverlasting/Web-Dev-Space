fc6d716ffc3e5ec71429558d4e57ddd98fddf717
EndeavorEverlasting <71802818+EndeavorEverlasting@users.noreply.github.com>
2023-07-24 19:28:24 -0400

    Files for the basic resume website
    These are the files I'd like to use across machines to design the 2-button (an anchor and a button) website that loads my resume. I use an anchor so users can use the middle mouse button to open the resume. I aim to learn how to add functionality to button elements so that users can use the middle mouse button on website buttons just like they would an anchor.

---
+++ Simple/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["C:\Users\MooMooMcgee\Desktop\workspace", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
